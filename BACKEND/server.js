0428d52fa243a3b249c2d6c3c041a05bd3556d5e
thilanRandika <dgtrandika@gmail.com>
2024-01-14 16:34:25 +0530

    create folders structure

---
+++ BACKEND/server.js
@@ -0,0 +1,35 @@
+const cors = require("cors");
+
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const app = express(); 
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8070;
+
+app.use(cors());
+app.use(bodyParser.json()); 
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useCreateIndex: true, // Use this option
+    // useFindAndModify: false
+});
+
+
+
+const connection = mongoose.connection;
+connection.once("open", ()=> {
+    console.log("Mongodb Connection Success!");
+
+})
+
+app.listen(PORT,() =>{
+
+    console.log(`Server is up and running on port number : ${PORT}`);
+})
