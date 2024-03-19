7316ee36a7876cc473f3ed9890c9959564bbeab3
it22591302 <sandeepkahawatta9@gmail.com>
2024-03-19 21:27:58 +0530

    fixed changers

--- BACKEND/routes/sellerPartnership/sellerProducts.js
+++ BACKEND/routes/sellerPartnership/sellerProducts.js
@@ -1,4 +1,4 @@
-//const Product = require("../../models/Product.js");
+const Product = require("../../models/inventory/Product.js");
 const SellerProducts = require("../../models/sellerPartnership/SellerProducts.js");
 const { verifySellerToOther } = require("../../utils/veryfyToken.js");
 
