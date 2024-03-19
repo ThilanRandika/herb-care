a857a200446e7250413f341f7db51d3c10e2b80d
it22591302 <sandeepkahawatta9@gmail.com>
2024-03-19 20:27:12 +0530

    create frontend seller partnership request component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,12 +6,14 @@ import {
 
 import './App.css';
 import Home from "./pages/common/home/Home";
+import Dashboard from "./pages/sellerPartnership/Home/Dashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>}></Route>
+        <Route path="/sellerPartnership" element={<Dashboard/>}></Route>
       </Routes>
     </BrowserRouter>
   );
