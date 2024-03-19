42823475eeb7e1a2dbc92b953d70acfc548f05fd
rasindu hansaka <153224872+RasinduHansaka@users.noreply.github.com>
2024-03-19 15:32:48 +0530

    New Emergency Branch Changes

---
+++ frontend/src/pages/inventory/Inventory_Dashboard/Inventory_Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AddProduct from '../../../components/inventory/AddProduct/AddProduct'
+
+function Inventory_Dashboard() {
+  return (
+    <div>Inventory_Dashboard
+
+        <AddProduct/>
+    </div>
+  )
+}
+
+export default Inventory_Dashboard
