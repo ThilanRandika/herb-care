74131a7f568ae05cbc0cefd285aac8fe4b77a5d8
Ishara Wijesundara <wijesundaraishara2001@gmail.com>
2024-04-08 01:09:03 +0530

    made changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import './App.css';
 import Home from "./pages/common/home/Home";
 import Inventory_Dashboard from "./pages/inventory/Inventory_Dashboard/Inventory_Dashboard";
 import Dashboard from "./pages/sellerPartnership/Home/Dashboard";
+import GiftPackage_manage from "./pages/staff/Dashboard/GiftPackage_manage";
 
 function App() {
   return (
@@ -16,9 +17,10 @@ function App() {
         <Route path="/" element={<Home/>}></Route>
         <Route path="/Inventory_Dashboard" element={<Inventory_Dashboard/>}></Route>
         <Route path="/sellerPartnership" element={<Dashboard/>}></Route>
+        <Route path="/staff" element={<GiftPackage_manage/>}></Route>
       </Routes>
-
     </BrowserRouter>
+
   );
 }
 
