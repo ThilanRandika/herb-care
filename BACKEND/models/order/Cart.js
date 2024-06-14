fe092048ae86ed710a73f4a433bcccc122c24159
Thilan Randika <128400496+ThilanRandika@users.noreply.github.com>
2024-06-14 18:36:55 +0530

    update cart model

--- BACKEND/models/order/Cart.js
+++ BACKEND/models/order/Cart.js
@@ -3,30 +3,34 @@ const mongoose = require('mongoose');
 
 // Define the schema for Cart
 const CartSchema = new mongoose.Schema({
-    product_id: {
-        type: mongoose.Types.ObjectId,
-        ref: "Product",
-        required: true,
-      },
-      name:{
-        type:String,
-        required: true,
-      },
-      quantity: {
-        type: Number,
-        required: true,
-      },
-      
-      totalPrice: {
-        type: Number,
-        required: true,
-      },
-      status: {
-        type: String,
-        required: true,
-        enum: ["inBag", "removed", "add To Order"],
-        default: "inBag",
-      }
+  customerId: {
+    type: String,
+    ref: "customer",
+    required: true,
+  },
+  product_id: {
+    type: mongoose.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  totalPrice: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+    enum: ["inBag", "removed", "add To Order"],
+    default: "inBag",
+  }
     },
     { timestamps: true }
 );
