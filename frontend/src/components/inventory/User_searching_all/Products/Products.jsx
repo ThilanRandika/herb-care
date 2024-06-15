f4027980d15223db1af46bc5d71bc2c80d798206
it22591302 <sandeepkahawatta9@gmail.com>
2024-06-15 18:19:22 +0530

    shop page css changes

--- frontend/src/components/inventory/User_searching_all/Products/Products.jsx
+++ frontend/src/components/inventory/User_searching_all/Products/Products.jsx
@@ -53,24 +53,69 @@ function Products({ searchQuery, priceRange, category }) {
     <div className="User-searching-card-container">
       
       {filteredProducts.map((product, index) => (
-        <Link to={`/Product/${product._id}`} key={index}>
-          <section className="User-searching-card">
-            <div className="productCard-img-inventory">
-              <img src={require(`../../../../../../BACKEND/uploads/${product.image}`)} className="card-img" alt={product.name} height={120} width={120}/>
-            </div>
 
-            <div className="User-searching-card-details">
-              <h3 className="User-searching-card-title1">{product.name}</h3>
+
+        // <Link to={`/Product/${product._id}`} key={index}>
+        //   <section className="User-searching-card">
+        //     <div className="productCard-img-inventory">
+        //       <img src={require(`../../../../../../BACKEND/uploads/${product.image}`)} className="card-img" alt={product.name} height={120} width={120}/>
+        //     </div>
+
+        //     <div className="User-searching-card-details">
+        //       <h3 className="User-searching-card-title1">{product.name}</h3>
               
-              <section className="User-searching-card-price">
-                <div className="User-searching-price">
-                  Rs. {product.price}
+        //       <section className="User-searching-card-price">
+        //         <div className="User-searching-price">
+        //           Rs. {product.price}
+        //         </div>
+        //       </section>
+        //       <button className="User-searching-add-to-cart-button1">Add to Cart</button>
+        //     </div>
+        //   </section>
+
+        
+          <div class="customer-product-list-card" key={index}>
+                <div class="customer-product-list-image">
+                  <img
+                    src={require(`../../../../../../BACKEND/uploads/${product.image}`)}
+                    className="customer-product-list-image"
+                    alt="Product"
+                  />
                 </div>
-              </section>
-              <button className="User-searching-add-to-cart-button1">Add to Cart</button>
-            </div>
-          </section>
-        </Link>
+                <div class="customer-product-list-details">
+                  <div class="customer-product-list-info1">
+                    <div class="customer-product-list-info2">
+                      <div class="customer-product-list-name">{product.name}</div>
+                      <div class="customer-product-list-description">{product.category}</div>
+                    </div>
+                    <div class="customer-product-list-price">
+                      Rs.{product.Manufactured_price}
+                    </div>
+                  </div>
+                  {/* <div class="customer-product-list-description">DESCRIPTION</div>
+              <div class="customer-product-list-description">
+                {product.description}
+              </div> */}
+                  <div className="customer-product-list-add-to-cart-button-container">
+                    <div>
+                      <Link to={`/Product/${product._id}`}>
+                        <button class="customer-product-list-add-to-cart-button">
+                          View Product
+                        </button>
+                      </Link>
+                    </div>
+                    <div className="customer-product-list-add-to-cart-cart-button">
+                      <img width="30" height="30" src="https://img.icons8.com/sf-regular-filled/48/1A1A1A/shopping-cart.png" alt="shopping-cart--v1"/>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+
+
+       // </Link>
+
+        
       ))}
     </div>
   );
